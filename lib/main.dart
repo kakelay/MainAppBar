b2aecdc2d38f923c4f98e1d6d8b8aa0b5ede8678
kakelay <kakelay18052002@gmail.com>
2024-05-20 12:10:01 +0700

    update code for app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,144 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: ProfilePage(),
+//     );
+//   }
+// }
+
+// class ProfilePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: MainAppBar(
+//         title: Text(
+//           'កែប្រែព័ត៌មាន',
+//           style: TextStyle(color: Colors.white),
+//         ),
+//       ),
+//       body: Stack(
+//         children: [
+//           Column(
+//             children: [
+//               Container(
+//                 height: 120,
+//                 color: Colors.transparent,
+//               ),
+//               Expanded(
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(30),
+//                       topRight: Radius.circular(30),
+//                     ),
+//                   ),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(16.0),
+//                     child: Column(
+//                       children: [
+//                         SizedBox(
+//                             height: 50), // To push content below the avatar
+//                         Text(
+//                           'តំលៃរយ:ឆ្លៀត',
+//                           style: TextStyle(
+//                             fontSize: 20,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         // Add more content here
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//           Positioned(
+//             top: 80,
+//             left: MediaQuery.of(context).size.width / 2 - 50,
+//             child: CircleAvatar(
+//               radius: 50,
+//               backgroundImage: NetworkImage(
+//                 'https://example.com/your-profile-image.jpg',
+//               ),
+//               child: Align(
+//                 alignment: Alignment.bottomRight,
+//                 child: CircleAvatar(
+//                   radius: 15,
+//                   backgroundColor: Colors.blue,
+//                   child: Icon(
+//                     Icons.camera_alt,
+//                     size: 15,
+//                     color: Colors.white,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   final Text title;
+//   final double barHeight = 50.0;
+
+//   MainAppBar({Key? key, required this.title}) : super(key: key);
+
+//   @override
+//   Size get preferredSize => Size.fromHeight(kToolbarHeight + 100.0);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return PreferredSize(
+//       child: ClipPath(
+//         clipper: WaveClip(),
+//         child: Container(
+//           color: Colors.blue,
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               SizedBox(height: kToolbarHeight), // Spacer to push the title down
+//               title,
+//             ],
+//           ),
+//         ),
+//       ),
+//       preferredSize: Size.fromHeight(kToolbarHeight + 100),
+//     );
+//   }
+// }
+
+// class WaveClip extends CustomClipper<Path> {
+//   @override
+//   Path getClip(Size size) {
+//     Path path = Path();
+//     final lowPoint = size.height - 30;
+//     final highPoint = size.height - 60;
+//     path.lineTo(0, size.height);
+//     path.quadraticBezierTo(size.width / 4, highPoint, size.width / 2, lowPoint);
+//     path.quadraticBezierTo(
+//         3 / 4 * size.width, size.height, size.width, lowPoint);
+//     path.lineTo(size.width, 0);
+//     return path;
+//   }
+
+//   @override
+//   bool shouldReclip(CustomClipper<Path> oldClipper) {
+//     return false;
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,71 +160,14 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: MainAppBar(
         title: Text(
-          'កែប្រែព័ត៌មាន',
+          'កែប្រែព័ត៌មានfdgfdg',
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              Container(
-                height: 120,
-                color: Colors.transparent,
-              ),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                            height: 50), // To push content below the avatar
-                        Text(
-                          'តំលៃរយ:ឆ្លៀត',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        // Add more content here
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Positioned(
-            top: 80,
-            left: MediaQuery.of(context).size.width / 2 - 50,
-            child: CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(
-                'https://example.com/your-profile-image.jpg',
-              ),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: CircleAvatar(
-                  radius: 15,
-                  backgroundColor: Colors.blue,
-                  child: Icon(
-                    Icons.camera_alt,
-                    size: 15,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+      body: Center(
+        child: Text(
+          'fdskfhsdjk',
+        ),
       ),
     );
   }
@@ -96,7 +180,7 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   MainAppBar({Key? key, required this.title}) : super(key: key);
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight + 100.0);
+  Size get preferredSize => Size.fromHeight(kToolbarHeight + 80.0);
 
   @override
   Widget build(BuildContext context) {
@@ -104,7 +188,7 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
       child: ClipPath(
         clipper: WaveClip(),
         child: Container(
-          color: Colors.blue,
+          color: Color.fromARGB(255, 0, 86, 235),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -126,9 +210,10 @@ class WaveClip extends CustomClipper<Path> {
     final lowPoint = size.height - 30;
     final highPoint = size.height - 60;
     path.lineTo(0, size.height);
-    path.quadraticBezierTo(size.width / 4, highPoint, size.width / 2, lowPoint);
     path.quadraticBezierTo(
-        3 / 4 * size.width, size.height, size.width, lowPoint);
+        size.width / 2.5, highPoint, (size.width / 2) + 42, lowPoint - 4);
+    path.quadraticBezierTo(
+        3 / 3.5 * size.width, size.height, size.width + 2, lowPoint + 28);
     path.lineTo(size.width, 0);
     return path;
   }
